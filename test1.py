404b35ff0ca5252863fbd200f5150107a747d13b
akumar6 <arundruk@gmail.com>
2020-03-09 21:01:07 +0530

    pgm_dict

--- test1.py
+++ test1.py
@@ -31,6 +31,32 @@
 
 ##############################################################################################################
 
-T=(1,2,(33,44))
-L=list(T)
-print(L,type(L))
+payload = {
+         "customerDetails" : {
+         "soldToCustomerNumber" : "5000871",
+         "billToCustomerNumber" : "",
+         "includeBasicFloorplans" : "FALSE",
+         "vehicleAttributes" : [ {
+         "vehicleReferenceId" : "7037088",
+         "vehicleYear" : "2018",
+         "maxOdometerReading" : "120",
+         "asIsIndicator" : "FALSE",
+         "totalVehicleDebitAmount" : "1000",
+         "salvageUnitIndicator" : "FALSE",
+         "salesChannel" : "RMS",
+         "vehicleType" : "V",
+         "saleType" : "O",
+         "hondaFloorplanThresholdFlag" : "TRUE",
+         "sellerAccount" : ""
+         }
+         ]
+         }
+         }
+
+L=payload["customerDetails"]["vehicleAttributes"]
+print(L[0]["vehicleReferenceId"])
+
+print(payload["customerDetails"]["vehicleAttributes"][0]["maxOdometerReading"])
+
+L1=payload["customerDetails"]["soldToCustomerNumber"]
+print(L1)
